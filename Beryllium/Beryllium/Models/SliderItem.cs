1b14608d6d1fff1591b7af045c49c6ac02f74f59
reshad <you@example.com>
2022-06-08 23:14:11 +0400

    first commit

---
+++ Beryllium/Beryllium/Models/SliderItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Beryllium.Models
+{
+    public class SliderItem
+    {
+        public int image { get; set; }
+        public int Title { get; set; }
+    }
+}
